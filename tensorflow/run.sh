17191ec65066b470d2e3fc8e5e90edbb7b9632e5
ryanjhkruger <57916334+ryanjhkruger@users.noreply.github.com>
2019-11-20 21:18:02 +0200

    Update run.sh

--- tensorflow/run.sh
+++ tensorflow/run.sh
@@ -15,7 +15,12 @@ fi
 
 echo "[Info] Install TensorFlow into deps"
 # shellcheck disable=SC2086
-if ! pip3 install --user --no-cache-dir --prefix= --no-dependencies https://storage.googleapis.com/tensorflow/raspberrypi/tensorflow-1.14.0-cp34-none-linux_armv7l.whl; then
+
+wget https://storage.googleapis.com/tensorflow/raspberrypi/tensorflow-1.14.0-cp34-none-linux_armv7l.whl
+mv tensorflow-1.14.0-cp34-none-linux_armv7l.whl tensorflow-1.14.0-cp36-none-linux_armv7l.whl
+sudo pip3 install tensorflow-1.11.0-cp36-none-linux_armv7l.whl
+
+if ! pip3 install --user --no-cache-dir --prefix= --no-dependencies tensorflow-1.14.0-cp36-none-linux_armv7l.whl; then
     echo "[Error] Can't install TensorFlow package!"
     exit 1
 fi
